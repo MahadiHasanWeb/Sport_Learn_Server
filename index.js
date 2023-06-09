df93f2589ba82eb941e9c48674181deca87c7994
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-06-10 01:19:58 +0600

    Approved role added

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.Access_Token, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwzfeis.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,18 +43,56 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const SportLearnUsersCollection = client.db('SportLearn').collection('users');
+        const ClassesCollection = client.db('SportLearn').collection('classes');
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.Access_Token, {
                 expiresIn: '1h'
             })
-            res.send(token)
+            res.send({ token })
         })
+
+
+
+        app.get('/classes', async (req, res) => {
+            const result = await ClassesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/classes', async (req, res) => {
+            const classes = req.body;
+            const result = await ClassesCollection.insertOne(classes);
+            res.send(result);
+        });
+
+        app.patch('/classes/approved/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'approved'
+                }
+            }
+            const result = await ClassesCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
 
         // User Apis
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await SportLearnUsersCollection.find().toArray();
             res.send(result);
         })
@@ -57,10 +111,25 @@ async function run() {
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
+            console.log(id)
             const query = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set: {
                     role: 'admin'
+                }
+            }
+            const result = await SportLearnUsersCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+
+        app.patch('/users/Instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'Instructor'
                 }
             }
             const result = await SportLearnUsersCollection.updateOne(query, updateDoc);
