8daade6c27003f7bc711936cc43cee34fcbd10bd
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-06-09 10:43:20 +0600

    User Delete Api Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -28,6 +29,46 @@ async function run() {
         const SportLearnUsersCollection = client.db('SportLearn').collection('users');
 
 
+        // User Apis
+
+        app.get('/users', async (req, res) => {
+            const result = await SportLearnUsersCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await SportLearnUsersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await SportLearnUsersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await SportLearnUsersCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await SportLearnUsersCollection.deleteOne(query)
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,7 +81,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Toys World Running!')
+    res.send('Sport Learn Running!')
 })
 
 app.listen(port, () => {
