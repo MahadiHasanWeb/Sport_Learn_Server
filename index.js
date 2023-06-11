d4f9bd136614075cbf661459814369bea0f5c93a
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-06-11 21:21:31 +0600

    Add SelectedClasses delete Api implement

--- index.js
+++ index.js
@@ -191,6 +191,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/selectedClass/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const deleted = await SelectedClassesCollection.deleteOne(query)
+            res.send(deleted)
+        })
 
 
 
